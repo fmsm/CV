d31906ef16da8bbe01fa04ed764def4a16c471cd
admin <admin@localhost.com>
2016-05-29 17:03:20 +0200

    Cambios en configuracion proyecto y .gitignore
    Para ver si asi deja de sucueder un error recurrente cada cierto tiempo.

--- src/main/webapp/resources/js/scripts.js
+++ src/main/webapp/resources/js/scripts.js
@@ -3,7 +3,7 @@ function showDlg(idDlg) {
 
 	var yBody = $('body').height();
 	var xBody = Math.floor( $('body').width() );
-	var xDlg = Math.floor( $('body').width() * 0.75);
+	var xDlg = Math.floor( $('body').width() * 0.66);
 					
 	$( idDlg ).height(yBody).width(0).css('left',xBody);
 	$( idDlg ).animate( { width: xDlg.toString(), left: '-=' + xDlg }, 400);
@@ -17,7 +17,7 @@ function hideDlg(idDlg, widgetVar) {
 					
 	$( idDlg ).animate( { 
 							left: '+=' + xBody.toString()},
-							400, function() {
+							600, function() {
 						 		PF( widgetVar ).hide(); 
 						});
 			
